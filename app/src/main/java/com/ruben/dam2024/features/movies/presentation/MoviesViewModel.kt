d738286652fa9fdcf292ddc79d010275f58059a1
ruben <ruben.vallejo.jara@gmail.com>
2024-10-01 17:47:26 +0200

    Cambios

---
+++ app/src/main/java/com/ruben/dam2024/features/movies/presentation/MoviesViewModel.kt
@@ -0,0 +1,21 @@
+package com.ruben.dam2024.features.movies.presentation
+
+import androidx.lifecycle.ViewModel
+import com.ruben.dam2024.features.movies.domain.GetMovieUseCase
+import com.ruben.dam2024.features.movies.domain.GetMoviesUseCase
+import com.ruben.dam2024.features.movies.domain.Movie
+
+class MoviesViewModel(
+    private val getMovieUseCase: GetMovieUseCase,
+    private val getMoviesUseCase: GetMoviesUseCase
+) : ViewModel() {
+    //Patron Model-View-View-Model MVVM
+
+    fun viewCreated(): List<Movie> {
+        return getMoviesUseCase.invoke()
+    }
+
+    fun movieSelected(id: String): Movie? {
+        return getMovieUseCase.invoke(id)
+    }
+}
